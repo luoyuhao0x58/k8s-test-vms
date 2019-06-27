cd2bec98e0b027b3f55a17abbfe39f5609ce5cb1
Luo Yuhao <luoyuhao@haujilo.xyz>
2019-06-27 10:36:26 +0800

    fix: 修复Helm安装完没有权限的问题。

--- scripts/centos7/install-helm.sh
+++ scripts/centos7/install-helm.sh
@@ -7,3 +7,10 @@ snap install helm --classic
 
 # https://helm.sh/docs/using_helm/#initialize-helm-and-install-tiller
 sudo -iuvagrant helm init --history-max 200
+
+# https://github.com/helm/helm/issues/3130#issuecomment-372931407
+sudo -iuvagrant kubectl --namespace kube-system create serviceaccount tiller
+sudo -iuvagrant kubectl create clusterrolebinding tiller-cluster-rule \
+  --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+sudo -iuvagrant kubectl --namespace kube-system patch deploy tiller-deploy \
+  -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
